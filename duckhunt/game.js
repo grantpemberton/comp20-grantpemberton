6f5522218a7978f6f3fb53c4b6ef5da4369d1ea0
grantpemberton <grant.pemberton@tufts.edu>
2015-10-13 22:49:22 -0400

    Wrote majority of game.js file

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,32 @@
-// Replace this with your code...
+function init (){
+
+	//From: https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Transformations
+	var ctx = document.getElementById('game_canvas').getContext('2d');
+	//create context for the loading the images
+	var img = new Image();
+	img.src = "duckhunt-background.gif";
+	//import background
+
+	var sheet = new Image();
+	sheet.src = "duckhunt_various_sheet.png";
+	//import sprite sheet
+
+	img.onload = function(){
+		ctx.drawImage(img, 0, 0);
+		//draw background first
+		ctx.drawImage(sheet, 0, 119, 40, 30, 60, 30, 40, 30);
+		//draws bird 1 by slicing sprite sheet
+		ctx.drawImage(sheet, 300, 119, 40, 30, 200, 100, 40, 30);
+		//draws bird 2 by slicing sprite sheet
+
+		
+	};
+}
+
+
+function wait(delay){
+	//enacts a while loop that delays program
+	mark = new Date();
+	//returns current date in milliseconds
+	while((new Date())-mark <= delay*1000){}
+}
